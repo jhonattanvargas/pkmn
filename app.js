b772037010e6318e0f8baaf86e959c19614ed969
jhonattan <jvargas@colegioandes.cl>
2018-04-20 12:01:58 -0300

    cors

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ const cors = require('cors')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
 app.use('/api',api)
-//app.use(cors())
-
+/*
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   })
-
+*/
 //Handlebars setup
 app.engine('hbs', exphbs({
     extname: 'hbs'
